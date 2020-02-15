3724dadc35d77c411d8faec98ad0c2e507808d56
Merlintor <merlinfuchs2001@gmail.com>
2020-02-15 16:50:40 +0100

    Add cache test command

--- modules/test.py
+++ modules/test.py
@@ -5,3 +5,8 @@ class TestModule(Module):
     @Module.command()
     async def yeet(self, ctx):
         await ctx.send("YEET!")
+
+    @Module.command()
+    async def channels(self, ctx):
+        channel = await ctx.get_channel()
+        await ctx.send(channel.name)

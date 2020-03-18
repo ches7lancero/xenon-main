0cd4ceb961e93a1d39a817b6d343843436e8119e
Merlintor <merlinfuchs2001@gmail.com>
2020-03-18 14:39:45 +0100

    Sync command strucutre

--- modules/sync.py
+++ modules/sync.py
@@ -1,5 +1,25 @@
 import xenon_worker as wkr
 
+import checks
+
 
 class Sync(wkr.Module):
-    pass
+    @wkr.Module.command()
+    @wkr.guild_only
+    @wkr.has_permissions(administrator=True)
+    @wkr.bot_has_permissions(administrator=True)
+    @checks.is_premium()
+    async def sync(self, ctx):
+        pass
+
+    @sync.command(aliases=("ls",))
+    async def list(self, ctx):
+        pass
+
+    @sync.command(aliases=("channels", "msg"))
+    async def messages(self, ctx):
+        pass
+
+    @sync.command()
+    async def bans(self, ctx):
+        pass

7fe9fc18aa553866c6d4c1c6444d3f18ebb8a1a5
Merlintor <merlinfuchs2001@gmail.com>
2020-03-23 17:43:04 +0100

    Change not-implemented message for the build command

--- modules/build.py
+++ modules/build.py
@@ -8,4 +8,5 @@ class Build(wkr.Module):
     @wkr.bot_has_permissions(administrator=True)
     @wkr.cooldown(1, 60, bucket=wkr.CooldownType.GUILD)
     async def build(self, ctx):
-        raise ctx.f.ERROR("This command is not re-implemented yet. Please give us some time.")
+        raise ctx.f.ERROR("This command is **currently not available**. It might come back in the future.\n"
+                          "Please don't ask us when it will come back, there is **no ETA**.")

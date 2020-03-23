a22ffcfa5533ce7dd5c89ead0c5d599bd1aca7f1
Merlintor <merlinfuchs2001@gmail.com>
2020-03-23 12:17:42 +0100

    create indexes and subscritptions for sync

--- modules/sync.py
+++ modules/sync.py
@@ -1,16 +1,29 @@
 import xenon_worker as wkr
+import pymongo
 
 import checks
 
 
 class Sync(wkr.Module):
+    @wkr.Module.listener()
+    async def on_load(self, *_, **__):
+        await self.bot.db.syncs.create_index([("target", pymongo.ASCENDING)])
+        await self.bot.db.syncs.create_index([("source", pymongo.ASCENDING)])
+
+        await self.bot.subscribe("*.message_create", shared=True)
+        await self.bot.subscribe("*.guild_ban_add", shared=True)
+        await self.bot.subscribe("*.guild_ban_remove", shared=True)
+
     @wkr.Module.command()
     @wkr.guild_only
     @wkr.has_permissions(administrator=True)
     @wkr.bot_has_permissions(administrator=True)
     @checks.is_premium()
     async def sync(self, ctx):
-        pass
+        """
+        Sync messages and bans between different servers and channels
+        """
+        await ctx.invoke("help sync")
 
     @sync.command(aliases=("ls",))
     async def list(self, ctx):

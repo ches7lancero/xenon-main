dae51512334b32a99a32f0051391f820726c2ee2
Merlintor <merlinfuchs2001@gmail.com>
2020-03-30 22:38:15 +0200

    Add notice for copy command

--- modules/copy.py
+++ modules/copy.py
@@ -2,4 +2,7 @@ import xenon_worker as wkr
 
 
 class Copy(wkr.Module):
-    pass
+    @wkr.Module.command(hidden=True)
+    async def copy(self, ctx):
+        raise ctx.f.INFO("The copy command is **currently not available**.\n"
+                         "Please create and then load a backup instead.")

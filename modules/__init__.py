3dd139b47c3ca6a22c0c14aa4d5c3ed163304834
Merlintor <merlinfuchs2001@gmail.com>
2020-06-25 09:18:08 +0200

    Remove build command

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,10 +3,9 @@ from .admin import Admin
 from .backups import Backups
 from .templates import Templates
 from .basics import Basics
-from .build import Build
 from .redis import Redis
 from .blacklist import Blacklist
 from .premium import Premium
 
 
-to_load = (Help, Admin, Backups, Basics, Templates, Build, Redis, Blacklist, Premium)
+to_load = (Help, Admin, Backups, Basics, Templates, Redis, Blacklist, Premium)
